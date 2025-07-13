50ab220f50953ed76455dd76a2084ba58545dfaa
Pi Lanningham <pi@sundae.fi>
2025-07-13 02:02:41 -0400

    Switch to def/indef encoding; add a test
    The sc's need to call cbor.serialise to check the signature, which doesn't respect the original encoding; so we need to match their conventions to make sure the signature stays valid

--- workers/dollar-cost-average/src/types.rs
+++ workers/dollar-cost-average/src/types.rs
@@ -92,3 +92,46 @@ pub fn serialize<T: AsPlutus>(value: T) -> Vec<u8> {
     minicbor::encode(value.to_plutus(), &mut bytes).expect("infallible");
     bytes
 }
+
+#[test]
+pub fn test_strategy_serialization() {
+    let (offer_policy_id, offer_asset_name) = (vec![], vec![]);
+    let (receive_policy_id, receive_asset_name) = (
+        hex::decode("99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e15").unwrap(),
+        hex::decode("534245525259").unwrap()
+    );
+
+    let validity_range = Interval {
+        lower_bound: IntervalBound {
+            bound_type: IntervalBoundType::Finite(1752270497000u64),
+            is_inclusive: true,
+        },
+        upper_bound: IntervalBound {
+            bound_type: IntervalBoundType::Finite(1752272897000u64),
+            is_inclusive: true,
+        },
+    };
+
+    let swap = Order::Swap {
+        offer: (offer_policy_id, offer_asset_name, 10000000),
+        min_received: (
+            receive_policy_id,
+            receive_asset_name,
+            1,
+        ),
+    };
+
+    let execution = StrategyExecution {
+        tx_ref: OutputReference {
+            transaction_id: TransactionId(hex::decode("da432ef16b7aa9b3972bdd42f86e6605c14444e75678f4e6fd75baa01168086f").unwrap()),
+            output_index: 0,
+        },
+        validity_range,
+        details: swap,
+        extensions: vec![],
+    };
+
+    let bytes = serialize(execution.clone());
+    let expected_bytes = hex::decode("d8799fd8799fd8799f5820da432ef16b7aa9b3972bdd42f86e6605c14444e75678f4e6fd75baa01168086fff00ffd8799fd8799fd87a9f1b00000197fb75e4e8ffd87a80ffd8799fd87a9f1b00000197fb9a83e8ffd87a80ffffd87a9f9f40401a00989680ff9f581c99b071ce8580d6a3a11b4902145adb8bfd0d2a03935af8cf66403e154653424552525901ffff40ff").unwrap();
+    assert_eq!(bytes, expected_bytes);
+}
