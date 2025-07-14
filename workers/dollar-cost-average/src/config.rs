198a9fbd1ee0cffb525c99aa01865f6e16d91df9
Pi Lanningham <pi@sundae.fi>
2025-07-13 20:16:34 -0400

    Iterate on surface area of strategies
    Still some more utilities I'd like to provide to trim things down, but this is a good start

---
+++ workers/dollar-cost-average/src/config.rs
@@ -0,0 +1,41 @@
+use balius_sdk::Error;
+use serde::Deserialize;
+
+use crate::utils::Network;
+
+#[derive(Deserialize)]
+pub struct DCAConfig {
+    pub network: Network,
+    pub interval: u64,
+    pub offer_token: String,
+    pub offer_amount: u64,
+    pub receive_token: String,
+    pub receive_amount_min: u64,
+}
+
+fn parse_token(token: &str) -> Option<(Vec<u8>, Vec<u8>)> {
+    let (policy_id, asset_name) = token.split_once(".")?;
+    Some((hex::decode(policy_id).ok()?, hex::decode(asset_name).ok()?))
+}
+
+impl DCAConfig {
+    pub fn offer_token(&self) -> Result<(Vec<u8>, Vec<u8>), Error> {
+        let Some((policy, name)) = parse_token(&self.offer_token) else {
+            return Err(Error::Internal(format!(
+                "Invalid offer token {}",
+                self.offer_token
+            )));
+        };
+        Ok((policy, name))
+    }
+
+    pub fn receive_token(&self) -> Result<(Vec<u8>, Vec<u8>), Error> {
+        let Some((policy, name)) = parse_token(&self.receive_token) else {
+            return Err(Error::Internal(format!(
+                "Invalid receive token {}",
+                self.receive_token
+            )));
+        };
+        Ok((policy, name))
+    }
+}
--- workers/dollar-cost-average/src/lib.rs
+++ workers/dollar-cost-average/src/lib.rs
@@ -1,170 +1,30 @@
+mod config;
 mod types;
 mod utils;
 
-use std::{collections::HashMap, time::Duration};
+use std::time::Duration;
 
-use balius_sdk::{
-    Ack, Config, Error, FnHandler, Json, Params, Tx, Utxo, UtxoMatcher, Worker, WorkerResult,
-    http::HttpRequest,
-};
+use std::fmt::Debug;
+use balius_sdk::{Ack, Config, Error, FnHandler, Tx, Utxo, UtxoMatcher, Worker, WorkerResult};
 use serde::{Deserialize, Serialize};
 use tracing::info;
 
 use crate::{
-    types::{
-        Interval, IntervalBound, Order, OrderDatum, OutputReference, SignedStrategyExecution,
-        StrategyAuthorization, StrategyExecution, TransactionId,
-    },
-    utils::{Network, kv},
+    types::{Interval, Order, OrderDatum, OutputReference, StrategyAuthorization, TransactionId},
+    utils::{keys::get_signer_key, kv},
 };
 
-#[derive(Deserialize)]
-struct MyConfig {
-    network: Network,
-    interval: u64,
-    offer_token: String,
-    offer_amount: u64,
-    receive_token: String,
-    receive_amount_min: u64,
-    valid_for_secs: f64,
-}
+pub const KV_SEEN_ORDERS: &'static str = "seen_orders";
 
-fn process_tx(config: Config<MyConfig>, tx: Tx) -> WorkerResult<Ack> {
-    info!("processing tx {}", hex::encode(tx.hash));
-    let spent_txos = tx
-        .tx
-        .inputs
-        .iter()
-        .map(|txi| (txi.tx_hash.to_vec(), txi.output_index as u64))
-        .collect::<Vec<_>>();
-
-    info!("Looking up seen orders");
-    let mut seen_orders: Vec<SeenOrderDetails> = kv::get("seen_orders")?.unwrap_or_default();
-    info!("seen_orders before spending: {:?}", seen_orders);
-
-    seen_orders.retain(|deets| {
-        spent_txos
-            .iter()
-            .all(|(hash, index)| !(&deets.tx_hash == hash && &deets.index == index))
-    });
-    kv::set("seen_orders", &seen_orders)?;
-    info!("retained orders: {:?}", seen_orders);
-
-    for seen in seen_orders {
-        let slot_passed = tx.block_slot - seen.slot;
-        if slot_passed > config.interval {
-            info!("trying to make a buy");
-            buy_buy_buy(&config, &seen)?;
-        } else {
-            info!(
-                "not yet, {} slots passed, out of {} interval; {} slots remaining...",
-                slot_passed,
-                config.interval,
-                config.interval - slot_passed
-            );
-        }
-    }
-
-    Ok(Ack)
-}
-
-fn buy_buy_buy(config: &MyConfig, order: &SeenOrderDetails) -> WorkerResult<()> {
-    let Some((offer_policy_id, offer_asset_name)) = parse_token(&config.offer_token) else {
-        return Err(Error::Internal(format!(
-            "Invalid offer token {}",
-            config.offer_token
-        )));
-    };
-    let Some((receive_policy_id, receive_asset_name)) = parse_token(&config.receive_token) else {
-        return Err(Error::Internal(format!(
-            "Invalid receive token {}",
-            config.receive_token
-        )));
-    };
-
-    info!("ignoring {}", config.valid_for_secs);
-    let now = config.network.to_unix_time(order.slot);
-    let valid_for = Duration::from_secs_f64(20. * 60.);
-    let validity_range = Interval {
-        lower_bound: IntervalBound {
-            bound_type: types::IntervalBoundType::Finite(now - valid_for.as_millis() as u64),
-            is_inclusive: true,
-        },
-        upper_bound: IntervalBound {
-            bound_type: types::IntervalBoundType::Finite(now + valid_for.as_millis() as u64),
-            is_inclusive: true,
-        },
-    };
-
-    let swap = Order::Swap {
-        offer: (offer_policy_id, offer_asset_name, config.offer_amount),
-        min_received: (
-            receive_policy_id,
-            receive_asset_name,
-            config.receive_amount_min,
-        ),
-    };
-
-    let execution = StrategyExecution {
-        tx_ref: OutputReference {
-            transaction_id: TransactionId(order.tx_hash.clone()),
-            output_index: order.index,
-        },
-        validity_range,
-        details: swap,
-        extensions: vec![],
-    };
-
-    let bytes = types::serialize(execution.clone());
-
-    let signature = balius_sdk::wit::balius::app::sign::sign_payload("default", &bytes)?;
-
-    let sse = SignedStrategyExecution {
-        execution,
-        signature: Some(signature),
-    };
-    let sse_bytes = types::serialize(sse);
-
-    let submit_sse = SubmitSSE {
-        tx_hash: hex::encode(&order.tx_hash),
-        tx_index: order.index,
-        data: hex::encode(&sse_bytes),
-    };
-
-    info!(
-        "posting to {}: {} / {} / {}",
-        config.network.relay_url(),
-        hex::encode(&order.tx_hash),
-        order.index,
-        hex::encode(&sse_bytes)
-    );
-
-    HttpRequest::post(config.network.relay_url())
-        .json(&submit_sse)?
-        .send()?;
-    Ok(())
-}
-
-fn parse_token(token: &str) -> Option<(Vec<u8>, Vec<u8>)> {
-    let (policy_id, asset_name) = token.split_once(".")?;
-    Some((hex::decode(policy_id).ok()?, hex::decode(asset_name).ok()?))
-}
-
-#[derive(Serialize)]
-struct SubmitSSE {
-    tx_hash: String,
-    tx_index: u64,
-    data: String,
-}
-
-fn process_order(_: Config<MyConfig>, utxo: Utxo<()>) -> WorkerResult<Ack> {
+fn watch_for_orders(_: Config<config::DCAConfig>, utxo: Utxo<()>) -> WorkerResult<Ack> {
     info!(
         slot = utxo.block_slot,
-        "utxo: {}.{}",
+        "utxo observed: {}.{}",
         hex::encode(&utxo.tx_hash),
         utxo.index
     );
 
+    // Check if it's a sundae v3 order datum?
     let Some(datum) = utxo
         .utxo
         .datum
@@ -172,10 +32,12 @@ fn process_order(_: Config<MyConfig>, utxo: Utxo<()>) -> WorkerResult<Ack> {
     else {
         return Ok(Ack);
     };
+
     let Some(key) = balius_sdk::get_public_keys().remove("default") else {
         return Err(Error::Internal("key not found".into()));
     };
 
+    // Check if it's *our* order
     if let Order::Strategy {
         auth: StrategyAuthorization::Signature { signer },
     } = &datum.details
@@ -187,43 +49,95 @@ fn process_order(_: Config<MyConfig>, utxo: Utxo<()>) -> WorkerResult<Ack> {
         return Ok(Ack);
     }
 
+    // Save this order in our key-value store
     let seen = SeenOrderDetails {
         slot: utxo.block_slot,
-        tx_hash: utxo.tx_hash,
-        index: utxo.index,
+        utxo: OutputReference {
+            transaction_id: TransactionId(utxo.tx_hash),
+            output_index: utxo.index,
+        },
     };
 
-    // track this for l8r
-    let mut all_seen: Vec<SeenOrderDetails> = kv::get("seen_orders")?.unwrap_or_default();
+    // This is an order "under our custody", so we hold onto it
+    let mut all_seen: Vec<SeenOrderDetails> = kv::get(KV_SEEN_ORDERS)?.unwrap_or_default();
     all_seen.push(seen);
-    kv::set("seen_orders", &all_seen)?;
-    info!("seen_orders: {all_seen:?}");
+    kv::set(KV_SEEN_ORDERS, &all_seen)?;
+
+    info!("tracking orders: {all_seen:?}");
 
     Ok(Ack)
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+fn process_tx(config: Config<config::DCAConfig>, tx: Tx) -> WorkerResult<Ack> {
+    info!("processing tx {}", hex::encode(tx.hash));
+    let spent_txos = tx
+        .tx
+        .inputs
+        .iter()
+        .map(|input| (input.tx_hash.to_vec(), input.output_index as u64))
+        .collect::<Vec<_>>();
+
+    info!("Marking orders as spent, if any...");
+    let mut seen_orders: Vec<SeenOrderDetails> = kv::get(KV_SEEN_ORDERS)?.unwrap_or_default();
+
+    seen_orders.retain(|spent| {
+        let (spent_hash, spent_index) = (&spent.utxo.transaction_id.0, &spent.utxo.output_index);
+        !spent_txos
+            .iter()
+            .any(|(hash, index)| spent_hash == hash && spent_index == index)
+    });
+
+    kv::set(KV_SEEN_ORDERS, &seen_orders)?;
+
+    info!("retained orders: {:?}", seen_orders);
+
+    for seen in seen_orders {
+        let slots_elapsed = tx.block_slot - seen.slot;
+        if slots_elapsed > config.interval {
+            info!("{} slots elapsed, triggering a buy order", slots_elapsed);
+            trigger_buy(&config, &seen)?;
+        } else {
+            info!(
+                "{} slots elapsed, out of {}; {} slots remaining before we trigger a buy...",
+                slots_elapsed,
+                config.interval,
+                config.interval - slots_elapsed
+            );
+        }
+    }
+
+    Ok(Ack)
+}
+
+fn trigger_buy(config: &config::DCAConfig, order: &SeenOrderDetails) -> WorkerResult<()> {
+    let (offer_policy_id, offer_asset_name) = config.offer_token()?;
+    let (receive_policy_id, receive_asset_name) = config.receive_token()?;
+
+    let now = config.network.to_unix_time(order.slot);
+    let valid_for = Duration::from_secs_f64(20. * 60.);
+    let validity_range = Interval::inclusive_range(
+        now - valid_for.as_millis() as u64,
+        now + valid_for.as_millis() as u64,
+    );
+
+    let swap = Order::Swap {
+        offer: (offer_policy_id, offer_asset_name, config.offer_amount),
+        min_received: (
+            receive_policy_id,
+            receive_asset_name,
+            config.receive_amount_min,
+        ),
+    };
+
+    utils::strategies::submit_execution(&config.network, &order.utxo, validity_range, swap)?;
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct SeenOrderDetails {
     slot: u64,
-    tx_hash: Vec<u8>,
-    index: u64,
-}
-
-fn get_signer_key(
-    _: Config<MyConfig>,
-    _: Params<HashMap<String, String>>,
-) -> WorkerResult<Json<SignerKey>> {
-    let Some(key) = balius_sdk::get_public_keys().remove("default") else {
-        return Err(Error::Internal("key not found".into()));
-    };
-    Ok(Json(SignerKey {
-        signer: hex::encode(key),
-    }))
-}
-
-#[derive(Serialize)]
-struct SignerKey {
-    signer: String,
+    utxo: OutputReference,
 }
 
 #[balius_sdk::main]
@@ -232,7 +146,7 @@ fn main() -> Worker {
 
     Worker::new()
         .with_request_handler("get-signer-key", FnHandler::from(get_signer_key))
+        .with_utxo_handler(UtxoMatcher::all(), FnHandler::from(watch_for_orders))
         .with_tx_handler(UtxoMatcher::all(), FnHandler::from(process_tx))
-        .with_utxo_handler(UtxoMatcher::all(), FnHandler::from(process_order))
         .with_signer("default", "ed25519")
 }
