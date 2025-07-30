e98c3e2b16029b648611786e5982881984135579
Pi Lanningham <pi@sundae.fi>
2025-07-30 16:35:21 -0400

    Improvements to the build script

--- build-workers.sh
+++ build-workers.sh
@@ -1,6 +1,9 @@
+ROOT="$(cd "$(dirname "$0")/" && pwd)"
+pushd $ROOT
 for worker in $(ls workers); do
     echo "Compile $worker"
     cargo run --bin balius-worker-builder -- \
         --source-dir "workers/$worker" \
         --target-file "balius-server/workers/$worker.wasm"
 done
+popd
